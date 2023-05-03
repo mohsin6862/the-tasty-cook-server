da64591df1482a57221fba533071cb946f36f18d
mohsin6862 <mohsinctg2206@gmail.com>
2023-05-03 22:37:47 +0600

    Send appy data

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 const recipes = require('./Data/RecipeData.json')
+const apply = require('./Data/ApplyAsChef.json')
 const cors = require('cors')
 app.use(cors());
 app.get('/', (req, res) => {
@@ -9,6 +10,9 @@ app.get('/', (req, res) => {
 })
 app.get('/recipes',(req,res)=>{
     res.send(recipes)
+})
+app.get('/apply', (req,res)=>{
+    res.send(apply)
 })
 
 app.listen(port, () => {
