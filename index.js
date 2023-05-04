c880d7cdd6827f24f8005d77784895740f8f025f
mohsin6862 <mohsinctg2206@gmail.com>
2023-05-04 15:08:29 +0600

    Dynamic ID created

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000;
 const recipes = require('./Data/RecipeData.json')
 const apply = require('./Data/ApplyAsChef.json')
+const features = require('./Data/Features.json')
 const cors = require('cors')
 app.use(cors());
 app.get('/', (req, res) => {
@@ -11,8 +12,17 @@ app.get('/', (req, res) => {
 app.get('/recipes',(req,res)=>{
     res.send(recipes)
 })
+
+app.get('/recipes/:id',(req,res)=>{
+    const id = req.params.id;
+    const selectedRecipe = recipes.find(n => n._id == id)
+    res.send(selectedRecipe)
+})
 app.get('/apply', (req,res)=>{
     res.send(apply)
+})
+app.get('/features',(req,res)=>{
+    res.send(features)
 })
 
 app.listen(port, () => {
